746eefc2a00711ad954e40e03077205c720bf68b
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-18 23:48:11 +0800

    add contact number

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,6 +18,42 @@ const Contact = () => {
       <p className="secondary-text">Bagi sebarang pertanyaan, sila hubungi</p>
       <div className="my-8 flex w-full flex-col items-center gap-4 px-4 font-semibold">
         <div className="flex w-full items-center">
+          <p className="w-3/5 text-start">Rizaman</p>
+          <div className="flex w-2/5 justify-end gap-6">
+            <a
+              href="tel:+60124666956"
+              className="text-3xl transition-colors duration-300 hover:text-[var(--button)]/90"
+            >
+              <IoCallOutline />
+            </a>
+            <a
+              href="https://wa.me/60124666956"
+              target="blank"
+              className="text-3xl transition-colors duration-300 hover:text-[var(--button)]/90"
+            >
+              <FaWhatsapp />
+            </a>
+          </div>
+        </div>
+        <div className="flex w-full items-center">
+          <p className="w-3/5 text-start">Nik Zailana</p>
+          <div className="flex w-2/5 justify-end gap-6">
+            <a
+              href="tel:+60134675460"
+              className="text-3xl transition-colors duration-300 hover:text-[var(--button)]/90"
+            >
+              <IoCallOutline />
+            </a>
+            <a
+              href="https://wa.me/60134675460"
+              target="blank"
+              className="text-3xl transition-colors duration-300 hover:text-[var(--button)]/90"
+            >
+              <FaWhatsapp />
+            </a>
+          </div>
+        </div>
+        <div className="flex w-full items-center">
           <p className="w-3/5 text-start">Izzatul Ain</p>
           <div className="flex w-2/5 justify-end gap-6">
             <a
